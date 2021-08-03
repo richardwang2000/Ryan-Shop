2b626055581efb2ed342e6c268456a126598b1ee
Ryan <ryanjiaming@gmail.com>
2021-08-03 15:46:55 -0400

    first commit

---
+++ backend/data.js
@@ -0,0 +1,88 @@
+import bcrypt from 'bcryptjs';
+
+const data = {
+
+  users: [
+    {
+      name: 'Dude',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: true,
+    },
+    {
+      name: 'Peep',
+      email: 'user@fake.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      name: 'Baby Dragon',
+      category: 'Pets',
+      image: '/images/p1.jpg',
+      price: 1000000,
+      countInStock: 10,
+      brand: 'RW',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Cute but fierce, your best companion!',
+    },
+    {
+      name: 'Adidas Fit Shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 100,
+      countInStock: 5,
+      brand: 'Adidas',
+      rating: 4.6,
+      numReviews: 10,
+      description: 'Nice hairdo!',
+    },
+    {
+      name: 'Lacoste Free Shirt',
+      category: 'Shirts',
+      image: '/images/p3.jpg',
+      price: 220,
+      countInStock: 5,
+      brand: 'Lacoste',
+      rating: 4.8,
+      numReviews: 17,
+      description: 'very FREE(totally)!',
+    },
+    {
+      name: 'Nike Slim Pant',
+      category: 'Pants',
+      image: '/images/p4.jpg',
+      price: 78,
+      countInStock: 7,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'slim for slim peeps not chunky peeps',
+    },
+    {
+      name: 'Puma Slim Pant',
+      category: 'Pants',
+      image: '/images/p5.jpg',
+      price: 65,
+      countInStock: 9,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'PUMA!',
+    },
+    {
+      name: 'Adidas Fit Pant',
+      category: 'Pants',
+      image: '/images/p6.jpg',
+      price: 139,
+      countInStock: 8,
+      brand: 'Adidas',
+      rating: 4.9,
+      numReviews: 15,
+      description: 'For fit people only',
+    },
+  ],
+};
+export default data;
